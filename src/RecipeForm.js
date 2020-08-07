4d4c77b707e6b2d81542a0aece8824fa1937f8cc
nsmith167 <n.smith167@gmail.com>
2020-08-07 15:23:58 -0400

    Not sure how long it's been. This is basically working now

--- src/RecipeForm.js
+++ src/RecipeForm.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.scss';
 import './RecipeForm.scss';
 import AppNavbar from './AppNavbar';
 import {Link, withRouter} from 'react-router-dom';
-import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Container, Form, FormGroup, Label, Input, Button, Col } from 'reactstrap'
 
 
 class RecipeForm extends Component {
 
     emptyRecipe = {
         name: '',
+        recipeType: 'Breakfast',
         instructions: '',
         ingredients: '',
     };
 
+    recipeTypeMap = {
+        BREAKFAST: "Breakfast",
+        AM_SNACK: "AM Snack",
+        LUNCH: "Lunch",
+        PM_SNACK: "PM Snack",
+        DINNER: "Dinner",
+        NIGHT_SNACK: "Night Snack"
+    }
+
     constructor(props) {
         super(props);
         this.state = {
-            recipe: this.emptyRecipe
+            recipe: this.emptyRecipe,
+            recipeTypes: []
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,6 +36,8 @@ class RecipeForm extends Component {
     }
 
     async componentDidMount() {
+        const returnedRecipeTypes = await (await fetch(`/recipetypes`)).json();
+        this.setState({recipeTypes: returnedRecipeTypes.map(type => this.recipeTypeMap[type])});
         if (this.props.match.params.id !== 'new') {
             const returnedRecipe = await (await fetch(`/recipes/recipe/${this.props.match.params.id}`)).json();
             this.setState({recipe: returnedRecipe});
@@ -59,6 +72,7 @@ class RecipeForm extends Component {
     async handleSubmit(event) {
         event.preventDefault();
         const {recipe} = this.state;
+        recipe.recipeType = Object.keys(this.recipeTypeMap).find(key => this.recipeTypeMap[key] === recipe.recipeType);
 
         await fetch('/recipes/recipe' + (recipe.id ? '/' + recipe.id : ''), {
             method: (recipe.id) ? 'PUT' : 'POST',
@@ -73,8 +87,9 @@ class RecipeForm extends Component {
     }
 
     render() {
-        const {recipe} = this.state;
-        const title = <h1>{recipe.id ? 'Edit Recipe' : 'New Recipe'}</h1>
+        const {recipe, recipeTypes} = this.state;
+        const title = <h1>{recipe.id ? 'Edit Recipe' : 'New Recipe'}</h1>;
+        const recipeTypeOptions = recipeTypes.map(type => <option>{type}</option>);
 
         return (
             <div>
@@ -86,6 +101,14 @@ class RecipeForm extends Component {
                             <Label for="inputName">Name</Label>
                             <Input type="text" name="name" id="inputName" onChange={this.handleChange} value={recipe.name || ''}/>
                         </FormGroup>
+                        <FormGroup row>
+                            <Col sm={4}>
+                                <Label for="typeSelect">Recipe Type</Label>
+                                <Input type="select" name="recipeType" id="typeSelect" onChange={this.handleChange} value={this.recipeTypeMap[recipe.recipeType]}>
+                                    {recipeTypeOptions}
+                                </Input>
+                            </Col>
+                        </FormGroup>
                         <FormGroup>
                             <Label for="inputInstructions">Instructions</Label>
                             <Input type="textarea" name="instructions" id="inputInstructions" onChange={this.handleChange} value={recipe.instructions || ''}/>
@@ -96,7 +119,7 @@ class RecipeForm extends Component {
                         </FormGroup>
                         <FormGroup>
                             <Button color="primary" className="saveButton" type="submit">Save</Button>
-                            <Button color="secondary" tag={Link} to={'/recipe/' + recipe.id + '/view'} className="cancelButton">Cancel</Button>
+                            <Button color="secondary" tag={Link} to={'/'} className="cancelButton">Cancel</Button>
                             <Button color="danger" className="deleteButton" onClick={this.handleDelete}>Delete</Button>
                         </FormGroup>
                     </Form>
--- src/RecipeView.js
+++ src/RecipeView.js
@@ -8,9 +8,19 @@ import { Container, Button } from 'reactstrap'
 class RecipeView extends Component {
     emptyRecipe = {
         name: '',
+        recipeType: '',
         instructions: '',
         ingredients: '',
     };
+
+    recipeTypeMap = {
+        BREAKFAST: "Breakfast",
+        AM_SNACK: "AM Snack",
+        LUNCH: "Lunch",
+        PM_SNACK: "PM Snack",
+        DINNER: "Dinner",
+        NIGHT_SNACK: "Night Snack"
+    }
 
     constructor(props) {
         super(props);
@@ -48,14 +58,17 @@ class RecipeView extends Component {
             <div>
                 <AppNavbar />
                 <Container>
-                    <h1>{recipe.name || 'Unnamed recipe'}</h1>
+                    <div class="recipe-header">
+                        <h1>{recipe.name || 'Unnamed recipe'}</h1>
+                        <h4 class="text-muted">{this.recipeTypeMap[recipe.recipeType]}</h4>
+                    </div>
                     <h3>Ingredients</h3>
                     <pre>{recipe.ingredients}</pre>
                     <h3>Instructions</h3>
                     <pre>{recipe.instructions}</pre>
                     <div>
                         <Button color="primary" tag={Link} to={'/recipe/' + recipe.id}>Edit</Button>
-                        <Button color="secondary" tag={Link} to='/recipes' className="cancelButton">Cancel</Button>
+                        <Button color="secondary" tag={Link} to={'/recipes'} className="cancelButton">Cancel</Button>
                         <Button color="danger" className="deleteButton" onClick={this.handleDelete}>Delete</Button>
                     </div>
                 </Container>
